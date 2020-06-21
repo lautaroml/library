2fbc195d93331d8c528ca5a41573d9ba2f4234de
Lautaro López <lautaroml@hotmail.com>
2020-06-21 11:06:45 -0300

    Set author_id attribute

--- app/Book.php
+++ app/Book.php
@@ -6,10 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+
     protected $guarded = [];
 
     public function path()
     {
         return '/books/' . $this->id;
     }
+
+    public function setAuthorIdAttribute($author)
+    {
+        $this->attributes['author_id'] = (Author::firstOrCreate([
+            'name' => $author
+        ]))->id;
+    }
+
 }
