77aa2483fbefe6699f730fd4aada09e69c3faad0
Lautaro López <lautaroml@hotmail.com>
2020-06-21 12:10:06 -0300

    Added checkout and checkin. Added reservations relationship

--- app/Book.php
+++ app/Book.php
@@ -21,4 +21,32 @@ class Book extends Model
         ]))->id;
     }
 
+    public function checkout($user)
+    {
+        $this->reservations()->create([
+            'user_id' => $user->id,
+            'checked_out_at' => now()
+        ]);
+    }
+
+    public function checkin($user)
+    {
+        $reservation = $this->reservations()
+            ->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+
+        if (!$reservation) throw new \Exception('The book was not checked ot');
+
+        $reservation->update([
+            'checked_in_at' => now()
+        ]);
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
 }
